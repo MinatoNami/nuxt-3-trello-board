502aebc9f357cdb0485c2a709dff80604386755e
MinatoNami <minato.nami.dev@gmail.com>
2024-04-16 15:16:06 +0800

    Add data persistence and update/delete for columns/tasks

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -2,40 +2,75 @@
 import type { Column, Task } from "@/types";
 import { nanoid } from "nanoid";
 import draggable from "vuedraggable";
-const columns = ref<Column[]>([
+const columns = useLocalStorage<Column[]>(
+  "trelloBoard",
+  [
+    {
+      id: nanoid(),
+      title: "Backlog",
+      tasks: [
+        {
+          id: nanoid(),
+          title: "Create marketing landing page",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: nanoid(),
+          title: "Add new section: Testimonials",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+    },
+    { title: "Selected for Development", id: nanoid(), tasks: [] },
+    { title: "In Progress", id: nanoid(), tasks: [] },
+    { title: "QA", id: nanoid(), tasks: [] },
+    { title: "Complete", id: nanoid(), tasks: [] },
+  ],
   {
-    id: nanoid(),
-    title: "Backlog",
-    tasks: [
-      {
-        id: nanoid(),
-        title: "Create marketing landing page",
-        createdAt: new Date(),
-        updatedAt: new Date(),
+    serializer: {
+      read: (value) => {
+        return JSON.parse(value).map((column: Column) => {
+          column.tasks = column.tasks.map((task: Task) => {
+            task.createdAt = new Date(task.createdAt);
+            return task;
+          });
+          return column;
+        });
       },
-      {
-        id: nanoid(),
-        title: "Add new section: Testimonials",
-        createdAt: new Date(),
-        updatedAt: new Date(),
+      write: (value) => {
+        return JSON.stringify(value);
       },
-    ],
-  },
-  { title: "Selected for Development", id: nanoid(), tasks: [] },
-  { title: "In Progress", id: nanoid(), tasks: [] },
-  { title: "QA", id: nanoid(), tasks: [] },
-  { title: "Complete", id: nanoid(), tasks: [] },
-]);
+    },
+  }
+);
 
 const alt = useKeyModifier("Alt");
 const handlePull = () => {
   const temp = alt.value ? "clone" : true;
   return temp;
 };
+
+const createColumn = () => {
+  const column: Column = {
+    id: nanoid(),
+    title: "",
+    tasks: [],
+  };
+  columns.value.push(column);
+  nextTick(() => {
+    (
+      document.querySelector(
+        ".column:last-of-type .title-input"
+      ) as HTMLInputElement
+    ).focus();
+  });
+};
 </script>
 
 <template>
-  <div>
+  <div class="flex items-start overflow-x-auto gap-4">
     <draggable
       v-model="columns"
       group="columns"
@@ -48,7 +83,17 @@ const handlePull = () => {
         <div class="column bg-gray-200 p-5 rounded min-w-[250px]">
           <header class="font-bold mb-4">
             <DragHandle />
-            {{ column.title }}
+            <input
+              class="title-input bg-transparent focus:bg-white rounded px-1 w-4/5"
+              @keyup.enter="($event.target as HTMLInputElement).blur()"
+              @keydown.backspace="
+                column.title === ''
+                  ? (columns = columns.filter((c) => c.id !== column.id))
+                  : null
+              "
+              type="text"
+              v-model="column.title"
+            />
           </header>
           <draggable
             v-model="column.tasks"
@@ -58,13 +103,26 @@ const handlePull = () => {
             handle=".drag-handle"
           >
             <template #item="{ element: task }: { element: Task }">
-              <div><TrelloBoardTask :task="task" /></div>
+              <div>
+                <TrelloBoardTask
+                  :task="task"
+                  @delete="
+                    column.tasks = column.tasks.filter((t) => t.id !== $event)
+                  "
+                />
+              </div>
             </template>
           </draggable>
           <footer><NewTask @add="column.tasks.push($event)" /></footer>
         </div>
       </template>
     </draggable>
+    <button
+      @click="createColumn"
+      class="bg-gray-200 whitespace-nowrap p-2 rounded opacity-50"
+    >
+      + Add Another Column
+    </button>
   </div>
 </template>
 
