5b5ada171d260f92dcf4d97aacc31984ef0878f3
MinatoNami <minato.nami.dev@gmail.com>
2024-04-16 15:22:07 +0800

    Remove vue school logo

--- app.vue
+++ app.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="p-10 h-[100vh] bg-teal-600 overflow-auto">
     <h1 class="text-4xl text-white flex items-center mb-10">
-      <img
-        width="200"
-        class="mr-3"
-        src="https://vueschool.io/img/logo/vueschool_logo_multicolor_negative.svg"
-        alt="Vue School"
-      />
-      Trello Board
+      Task Management Board
     </h1>
     <TrelloBoard />
   </div>
